990002c90a635ce5671bfe0b47d505a6098728dd
Lee-Hyunkyu94 <79684577+Lee-Hyunkyu94@users.noreply.github.com>
2021-03-20 17:14:51 +0900

    0320

--- gradeBook/Student.java
+++ gradeBook/Student.java
@@ -1,66 +1,78 @@
 package gradeBook;
 
-import board.User;
+// 학생 클래스(학생 번호, 이름, 소속 반, 점수 입력 여부, 국어 점수, 영어 점수, 수학 점수)
 
 public class Student {
     // 필드
-    private int studentId;
-
-    private String LogId;
-    private String LogPw;
-    private String studentName;
-    private int studentClassRoom;
-    private boolean studentInputChecker;
+    // 학생 번호
+    private int id;
+    // 학생 로그인시 사용할 아이디
+    private String username;
+    // 학생 로그인시 사용할 비밀번호
+    private String password;
+    // 학생이름
+    private String name;
+    // 소속 반
+    private int group;
+    // 점수 입력 여부
+    private boolean scoreInputSwitch;
+    // 국어 점수
     private int korean;
+    // 영어 점수
     private int english;
+    // 수학 점수
     private int math;
 
-    // 메서드
-    public int getStudentId() {
-        return studentId;
+    // 과목 숫자를 저장한 상수
+    private final int SUBJECT_SIZE = 3;
+
+    // 필드에 대한 getter/setter 메서드
+    public int getId() {
+        return id;
     }
 
-    public void setStudentId(int studentId) {
-        this.studentId = studentId;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public String getLogId() {
-        return LogId;
-    }
-    
-    public void setLogId(String logId) {
-        LogId = logId;
-    }
-    
-    public String getLogPw() {
-        return LogPw;
-    }
-    
-    public void setLogPw(String logPw) {
-        LogPw = logPw;
-    }
-    public String getStudentName() {
-        return studentName;
+    public String getUsername() {
+        return username;
     }
 
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public int getStudentClassRoom() {
-        return studentClassRoom;
+    public String getPassword() {
+        return password;
     }
 
-    public void setStudentClassRoom(int studentClassRoom) {
-        this.studentClassRoom = studentClassRoom;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public boolean isStudentInputChecker() {
-        return studentInputChecker;
+    public String getName() {
+        return name;
     }
 
-    public void setStudentInputChecker(boolean studentInputChecker) {
-        this.studentInputChecker = studentInputChecker;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public boolean isScoreInputSwitch() {
+        return scoreInputSwitch;
+    }
+
+    public void setScoreInputSwitch(boolean scoreInputSwitch) {
+        this.scoreInputSwitch = scoreInputSwitch;
     }
 
     public int getKorean() {
@@ -92,20 +104,42 @@ public class Student {
 
         if (o instanceof Student) {
             Student s = (Student) o;
-            if (studentId == s.studentId) {
+            if (id == s.id) {
                 return true;
             }
         }
         return false;
     }
-    
-    public boolean logIn(Student s) {
-        if(LogId.equals(s.LogId) && LogPw.equals(s.LogPw)) {
+
+    // 학생의 정보를 출력한 print()
+    // 단 민감한 정보들인 아이디와 비밀번호는 출력하지 않는다.
+    public void print() {
+        System.out.println("+++++++++++++++++++++++++++++++++++");
+        System.out.printf("소속 반: %d반 번호: %2d번", group, id);
+        System.out.printf("이름: %s\n", name);
+        // 점수 입력 여부는 따로 추력을 해주는 것이 아니라
+        // 점수 출력 자체를 결정 지어주는데 사용된다.
+        System.out.println("-----------------------------------");
+        if (scoreInputSwitch) {
+            int sum = korean + english + math;
+            double average = sum / (double) SUBJECT_SIZE;
+            System.out.printf("국어 점수: %03d점 영어 점수: %03d점 수학 점수: %03d점\n", korean, english, math);
+            System.out.printf("총점: %03d점 평균: %06.2f점\n", sum, average);
+        } else {
+            System.out.println();
+            System.out.println("아직 입력된 정보가 없습니다.");
+            System.out.println();
+        }
+        System.out.println("+++++++++++++++++++++++++++++++++++");
+    }
+
+    public boolean auth(Student s) {
+        if (username.equals(s.username) && password.equals(s.password)) {
             return true;
         }
-            
+
         return false;
-        
+
     }
 
 }
