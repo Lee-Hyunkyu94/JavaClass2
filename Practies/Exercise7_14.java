14abebdf9a68ff619760eef3b2e332a4012306d2
Lee-Hyunkyu94 <79684577+Lee-Hyunkyu94@users.noreply.github.com>
2021-04-01 20:51:34 +0900

    Update Exercise7_14.java

--- Practies/Exercise7_14.java
+++ Practies/Exercise7_14.java
@@ -1,5 +1,29 @@
 package exercise;
 
-public class Exercise7_14 {
+class SutdaCard2 {
+    final int NUM;
+    final boolean IS_KWANG;
 
+    SutdaCard2() {
+        this(1, true);
+    }
+
+    SutdaCard2(int num, boolean isKwang) {
+        this.NUM = num;
+        this.IS_KWANG = isKwang;
+    }
+
+    public String toString() {
+        return NUM + (IS_KWANG ? "K" : "");
+    }
+
+    
+
+}
+
+public class Exercise7_14 {
+    public static void main(String[] args) {
+        SutdaCard2 card = new SutdaCard2(1, true);
+        System.out.println(card);
+    }
 }
