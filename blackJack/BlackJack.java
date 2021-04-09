64598c324c1840257a05e436cf637bdf0a532b8f
Lee-Hyunkyu94 <79684577+Lee-Hyunkyu94@users.noreply.github.com>
2021-04-09 17:26:16 +0900

    Update BlackJack.java

--- blackJack/BlackJack.java
+++ blackJack/BlackJack.java
@@ -7,29 +7,34 @@ import util.ScannerUtil;
 public class BlackJack {
     
     Scanner sc = new Scanner(System.in);
+    Card card;
     
     public void play() {
-        Dealer dealer = new Dealer();
-        Gamer gamer = new Gamer();
-        Rule rule = new Rule();
-        CardDeck cardDeck = new CardDeck();
-        
-        
+       System.out.println("=============BlackJack============");
+       Scanner sc = new Scanner(System.in);
+       
+       
+       Gamer gamer = new Gamer();
+       Dealer dealer = new Dealer();
+     
+       CardDeck cardDeck = new CardDeck();
+       
+       PlayingGame(sc,cardDeck,gamer,dealer);
+       
     }
     
-    public void PlayingGame(CardDeck cardDeck, Gamer gamer, Dealer dealer, Rule rule) {
+    private void PlayingGame(Scanner sc, CardDeck cardDeck, Gamer gamer, Dealer dealer) {
+        
         while(true) {
             System.out.println("----------------------");
             String message = new String("1. 게임시작 2. 종료");
             int userChoice = ScannerUtil.nextInt(sc, message, 1, 2);
             
             if(userChoice == 1) {
-                Card card = cardDeck.selectCard();
-                gamer.getCard(card);
-                dealer.printCards();
-                gamer.printCards();
-                rule.winorlose();
-                rule.accountscore();
+                card = cardDeck.selectCard();
+                gamer.receiveCard(card);
+                card = cardDeck.selectCard();
+                dealer.receiveCard(card);
             } else if(userChoice == 2) {
                 break;
             }
