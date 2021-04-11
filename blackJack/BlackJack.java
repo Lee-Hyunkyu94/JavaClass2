66e81097534cb5ee81c49327efa0ee1beab4480c
Lee-Hyunkyu94 <79684577+Lee-Hyunkyu94@users.noreply.github.com>
2021-04-11 20:36:53 +0900

    Update BlackJack.java

--- blackJack/BlackJack.java
+++ blackJack/BlackJack.java
@@ -1,43 +1,61 @@
 package blackJack;
-
 import java.util.Scanner;
-
-import util.ScannerUtil;
 
 public class BlackJack {
     
     Scanner sc = new Scanner(System.in);
     Card card;
+    CardDeck cardDeck;
     
     public void play() {
        System.out.println("=============BlackJack============");
        Scanner sc = new Scanner(System.in);
-       
-       
        Gamer gamer = new Gamer();
        Dealer dealer = new Dealer();
-     
        CardDeck cardDeck = new CardDeck();
-       
        PlayingGame(sc,cardDeck,gamer,dealer);
        
     }
     
     private void PlayingGame(Scanner sc, CardDeck cardDeck, Gamer gamer, Dealer dealer) {
-        
+        String gamerInput, dealerInput;
+        boolean gamerturn = false , dealerturn = false;
         while(true) {
-            System.out.println("----------------------");
-            String message = new String("1. 게임시작 2. 종료");
-            int userChoice = ScannerUtil.nextInt(sc, message, 1, 2);
+            System.out.println("카드를 더 뽑게요? 그럼 1번 싫으면 0번");
+            gamerInput = sc.nextLine();
             
-            if(userChoice == 1) {
-                card = cardDeck.selectCard();
+            if("0".equals(gamerInput)) {
+               gamerturn = true;
+            }else {
+                Card card = cardDeck.selectCard();
                 gamer.receiveCard(card);
-                card = cardDeck.selectCard();
+            }
+            
+            System.out.println("카드를 더 뽑게요? 그럼 1번 싫으면 0번");
+            dealerInput = sc.nextLine();
+            
+            if("0".equals(dealerInput)) {
+                dealerturn = true;
+            }else {
+                Card card = cardDeck.selectCard();
                 dealer.receiveCard(card);
-            } else if(userChoice == 2) {
+            }
+            
+            if(gamerturn&&dealerturn) {
                 break;
             }
+            }
+    }
+    
+    private void firstgameinit(CardDeck cardDeck, Gamer gamer, Dealer dealer) {
+        System.out.println("처음 2장의 카드를 줄게요.");
+        for(int i = 0; i < 2;i++) {
+            Card card = cardDeck.selectCard();
+            gamer.receiveCard(card);
+            
+            Card card2 = cardDeck.selectCard();
+            dealer.receiveCard(card2);
+                    
         }
     }
 }
