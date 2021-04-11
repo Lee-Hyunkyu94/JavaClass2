711927921eed5f87239f7208e8b98298618e6dd8
Lee-Hyunkyu94 <79684577+Lee-Hyunkyu94@users.noreply.github.com>
2021-04-11 20:39:19 +0900

    Update Main.java

--- blackJack/Main.java
+++ blackJack/Main.java
@@ -4,11 +4,12 @@ public class Main {
     public static void main(String[] args) {
         BlackJack bj = new BlackJack();
         
-        Dealer dealer = new Dealer();
-        Gamer gamer = new Gamer();
-        Rule rule = new Rule();
-        CardDeck cardDeck = new CardDeck();
+        //Dealer dealer = new Dealer();
+        //Gamer gamer = new Gamer();
+        //Rule rule = new Rule();
+        //CardDeck cardDeck = new CardDeck();
         
-        bj.PlayingGame(cardDeck, gamer, dealer, rule);
+        bj.play();
+        
     }
 }
